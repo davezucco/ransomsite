635c2ecc5f58d4adc5e38b1d4185b59b5d2b83b0
David <davidzucco22@gmail.com>
2018-11-30 10:06:44 -0500

    Updates

---
+++ precache-manifest.f2e968e80073cb097f382d4b5979f184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ransomsite/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "14a5a86b144e06191471",
+    "url": "/ransomsite/static/js/runtime~main.14a5a86b.js"
+  },
+  {
+    "revision": "938dc78e8acc8eacbd84",
+    "url": "/ransomsite/static/js/main.938dc78e.chunk.js"
+  },
+  {
+    "revision": "2bb5c567f4d338691ef7",
+    "url": "/ransomsite/static/js/1.2bb5c567.chunk.js"
+  },
+  {
+    "revision": "938dc78e8acc8eacbd84",
+    "url": "/ransomsite/static/css/main.e14461ee.chunk.css"
+  },
+  {
+    "revision": "09896dace0a3a29e99f58d2b7ca61e9e",
+    "url": "/ransomsite/index.html"
+  }
+];
